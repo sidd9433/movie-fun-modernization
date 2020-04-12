70b18c4bba42610e2d88ccce352f256c1776a9e4
“Siddhartha <siddhartha.mukherjee@rabobank.nl>
2020-04-12 22:17:25 +0200

    Service Discovery

--- applications/movie-fun-app/src/main/java/org/superbiz/moviefun/DevelopmentSecurityConfig.java
+++ applications/movie-fun-app/src/main/java/org/superbiz/moviefun/DevelopmentSecurityConfig.java
@@ -1,6 +1,7 @@
 package org.superbiz.moviefun;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,6 +21,7 @@ public class DevelopmentSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
+    @LoadBalanced
     public RestOperations restOperations() {
         return new RestTemplate();
     }
